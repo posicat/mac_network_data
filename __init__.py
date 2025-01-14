f51015e2fefc985a1ca0572b3e9ae140114e3721
Mark <posicat@pobox.com>
2025-01-14 00:31:32 -0600

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,23 @@
+import json
+import os
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "network_data"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Network Data integration."""
+    hass.data[DOMAIN] = {}
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Network Data from a config entry."""
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    return True
