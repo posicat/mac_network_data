ba35ddac07cd763d6d4bbc377e4dc00f42d2c72c
PosiCat <posicat@pobox.com>
2025-02-16 02:32:32 -0600

    Trial #123172312

--- custom_components/mac_network_data/__init__.py
+++ custom_components/mac_network_data/__init__.py
@@ -5,7 +5,6 @@ import logging
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from .sensor import async_setup_platform
 from .constants import DOMAIN, LOGGER
 
 async def async_setup(hass: HomeAssistant, config: dict):
@@ -15,11 +14,10 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up the mac_network_data integration when a config entry is created."""
-    config = entry.data
-    
-    # Here, instead of using discovery.load_platform, directly call async_setup_platform
-    await async_setup_platform(hass, config, async_add_entities, discovery_info=None)
-
+    # Forward the setup to the sensor platform
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
