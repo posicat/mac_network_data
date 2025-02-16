2cc276005f1a3b518125fa10feedfb39815166c1
Mark <posicat@pobox.com>
2025-02-16 01:28:07 -0600

    Update __init__.py

--- custom_components/mac_network_data/__init__.py
+++ custom_components/mac_network_data/__init__.py
@@ -2,9 +2,9 @@ import json
 import os
 import logging
 
-from homeassistant.config_entries import ConfigEntry
+from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
-
+from .sensor import async_setup_platform
 from .constants import DOMAIN, LOGGER
 
 async def async_setup(hass: HomeAssistant, config: dict):
@@ -13,7 +13,13 @@ async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up Network Data from a config entry."""
+    """Set up the mac_network_data integration."""
+    # This will register the platform with Home Assistant
+    hass.data[DOMAIN] = {}
+
+    # Register platform
+    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
